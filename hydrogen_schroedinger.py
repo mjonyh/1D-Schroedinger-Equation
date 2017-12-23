5f15040c6af72305f9b31d9b49346c226d99dd52
mjonyh <mjonyh@gmail.com>
2017-12-23 21:53:19 +0600

    restructured and cleanup

--- hydrogen_schroedinger.py
+++ hydrogen_schroedinger.py
@@ -5,6 +5,23 @@ from classes.class_secant import Secant_Method
 from classes.class_numerov import Numerov_Method
 from classes.class_simpson import Simpson_Method
 
+'''
+Please provide the input value for solving SE of Hydrogen
+Atom
+=========================================================
+l is the potential parameter
+eigen_min is the lowest probable eigen value
+potential_initial_position is the lowest position of potential
+potential_final_position is the highest position of potential
+
+'''
+
+l = 0.0
+
+number_of_eigen = 5
+eigen_min = -10
+potential_initial_position = 0.01
+potential_final_position = 20.0
 
 '''
 Potential function of the quantum system.
@@ -14,7 +31,6 @@ Potential function of the quantum system.
 
 def v(x):
     'potential for hydrogen atom'
-    l = 0.0
 
     return -1.0/x + l * (l + 1.0)/(x*x)
 
@@ -27,13 +43,13 @@ Help function to discretization of the analytical form
 
 def anal_disc():
     'Discretization of the analytical form will be done'
-    initial = 0.01
+    initial = potential_initial_position
     h = 0.01
 
     x = []
     y = []
 
-    while initial <= 20:
+    while initial <= potential_final_position:
         x.append(initial)
         y.append(v(initial))
         initial += h
@@ -68,6 +84,11 @@ def probability(u):
     for i in range(len(u)):
         y.append(u[i] * u[i])
 
+    result = integration.simpson(y)
+
+    for i in range(len(u)):
+        y[i] = y[i]/result
+
     return y
 
 
@@ -77,11 +98,11 @@ Function used to make list of the eigenenery to plot.
 '''
 
 
-def eigen(x, l):
+def eigen(x, eigen_value):
     'Making the list of an eigenvalue for plotting'
     y = []
     for i in range(len(x)):
-        y.append(l)
+        y.append(eigen_value)
 
     return y
 
@@ -120,44 +141,62 @@ find = Secant_Method(wavefunction.numerov)
 'Calling Simpson Method to integrate'
 integration = Simpson_Method()
 
-'Initial guess for eigen value'
-l = -10
+# 'Initial guess for eigen value'
+# l = eigen_min
 
 title = 'Eigenvalue(s) = '
 
-while l < 0:
+y_min = 0
+y_max = 0
+count = 0
+while count < number_of_eigen:
+    eigen_bak = eigen_min
+
     'Searching for the eigenvalue.'
-    l = find.secant(l)
+    l = find.secant(eigen_min)
 
-    title = title + str("%.2f" % round(l, 2)) + ', '
+    if(l != eigen_bak):
+        title = title + str("%.3f" % round(l, 3)) + ', '
 
-    'Calculating the eigenfunction.'
-    x, u = wavefunction.numerov(l)
+        'Calculating the eigenfunction.'
+        x, u = wavefunction.numerov(l)
 
-    'Normalizing the eigenfunction'
-    u = normalized(u)
+        'Normalizing the eigenfunction'
+        u = normalized(u)
 
-    'Probability of eigenfunction'
-    u = probability(u)
+        'Probability of eigenfunction'
+        p = probability(u)
 
-    'list to plot eigenvalue'
-    el = eigen(x, l)
+        'list to plot eigenvalue'
+        el = eigen(x, l)
 
-    'Adding eigenvalue and normalized wavefunction'
-    n_u = mixfunction(u, el)
+        'Adding eigenvalue and normalized wavefunction'
+        n_u = mixfunction(u, el)
 
-    plt.plot(x, n_u, 'g')
-    plt.plot(x, el, 'r')
+        'Adding eigenvalue and probability function'
+        n_p = mixfunction(p, el)
+
+        plt.plot(x, n_u, 'b')
+        plt.plot(x, n_p, 'g')
+        plt.plot(x, el, 'r')
+
+    eigen_min = l/3.0
+
+    if(min(n_u) < y_min):
+        y_min = min(n_u)
+
+    if(max(n_p) > y_max):
+        y_max = max(n_p)
 
     'for loop'
-    l += 0.3
+    count += 1
 
 'Plotting graph'
 plt.figure(1)
 plt.xlabel('Position x')
 plt.ylabel('Magnitude')
 plt.title(title)
-plt.legend(['$V(x)$', 'p(x)', '$E_n$'], loc=4)
-plt.axis([0, 20, -1, 0.5])
+plt.legend(['$V(x)$', '$\phi (x)$', '$Probability(x)$', '$E_n$'], loc=4)
+plt.axis([potential_initial_position, potential_final_position, y_min - 0.1, y_max + 0.1])
 
 plt.show()
